f1b166f5da29d6012d75db18d1a18e10fe961688
mygitversion <vn968y@gmail.com>
2021-11-29 21:18:08 +0300

    added /planet command and time in logging

--- bot.py
+++ bot.py
@@ -10,34 +10,39 @@ import ephem
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 
-logging.basicConfig(filename='bot.log', level=logging.INFO)  
+logging.basicConfig(filename='bot.log', \
+                    format = '%(asctime)s %(levelname)s %(message)s', \
+                    datefmt='%m/%d/%Y %I:%M:%S %p',  \
+                    level=logging.INFO)  
 
 def greet_user(update, context):
-    print("Вызван /start") 
     update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
 
 def talk_to_me(update, context):
     user_text = update.message.text 
-    print(user_text)
     update.message.reply_text(user_text) 
 
 def planet_where(update, context):
     user_text = update.message.text
-    print('user text', user_text)
     user_text_list = user_text.strip().split() 
-    print('user text list', user_text_list)
-    bool0 = len(user_text_list) == 2
-    bool1 = user_text_list[0] == '/planet'
+        
+    if len(user_text_list) == 0 or user_text_list[0] != '/planet':
+        return
+    
+    msge = "is wrong planet name"
+    if len(user_text_list) == 1:
+        update.message.reply_text('no name ' + msge)
+        return
+    
     planet_name = user_text_list[1].lower().capitalize()
-    bool2 = planet_name in ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
-    print('bool0 and bool1 and bool2:', bool0 and bool1 and bool2)
-    if bool0 and bool1 and bool2:
+    planet_name_valid = planet_name in ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
+    if planet_name_valid:
         ephem_planet = getattr(ephem, planet_name)()
-        ephem_planet.compute('2021/11/29 07:30:12')
+        ephem_planet.compute('2021/12/01 07:30:12')
         msge = ephem.constellation(ephem_planet)
     else:
-        msge = "The planet name doesn't exist"
-    print(msge)
+        msge = planet_name + ' ' + msge
+    
     update.message.reply_text(msge)
 
 def main():
@@ -50,7 +55,7 @@ def main():
     #dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     
     # Командуем боту начать ходить в Telegram за сообщениями
-    logging.info("Бот стартовал")   # to do: добавить дату и время в логи
+    logging.info("Бот стартовал")  
     mybot.start_polling()
     # Запускаем бота, он будет работать, пока мы его не остановим принудительно
     mybot.idle()
